d8a92588db5c65f24736fd21a696360106c84825
Tetiana Peleshko <t.peleshko@gmail.com>
2023-07-17 00:58:53 +0300

    Homework3

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
+//Task 1
+let first = prompt('Enter the first line');
+let second = prompt(`Enter the second line`);
+let third = prompt(`Enter the third line`);
+let concatResult = first.concat(" ", second, " ", third);
+alert(concatResult);
+
+
+//Task 2
 let name = prompt("What's your name?");
-alert("Hello, "+name+"! How are you?");
+let age = prompt("How old are you?");
+alert(`Вітаю ${name}\nваш вік ${age}`);
