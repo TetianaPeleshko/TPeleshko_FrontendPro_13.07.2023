13fd85e0b82e77cd86d5043ba5461ac1280b1207
Tetiana Peleshko <t.peleshko@gmail.com>
2023-07-17 18:42:21 +0300

    Changed js file Homework3 according to the comment

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 //Task 1
 let first = prompt('Enter the first line');
-let second = prompt(`Enter the second line`);
-let third = prompt(`Enter the third line`);
-let concatResult = first.concat(" ", second, " ", third);
+let second = prompt('Enter the second line');
+let third = prompt('Enter the third line');
+let concatResult = first.concat(' ', second, ' ', third);
 alert(concatResult);
 
 
 //Task 2
-let name = prompt("What's your name?");
-let age = prompt("How old are you?");
-alert(`Вітаю ${name}\nваш вік ${age}`);
+let name = prompt('What`s your name?');
+let age = prompt('How old are you?');
+alert(`Вітаю ${name}
+ваш вік ${age}`);
