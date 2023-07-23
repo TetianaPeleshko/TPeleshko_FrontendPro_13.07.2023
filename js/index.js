64eea3c2b9b8654b532398ffb3e69f0107393352
Tetiana Peleshko <t.peleshko@gmail.com>
2023-07-23 20:57:56 +0300

    HW8_Array

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
-let numOrStr = prompt('Введіть число або рядок');
-console.log(numOrStr);
+let length = parseInt(prompt('Enter the length of the array greater than 0:'));
+let arr = [];
 
-switch (true) {
-  case numOrStr === null:
-    console.log('Ви скасували');
-    break;
-  case numOrStr.trim() === '':
-    console.log('Порожній рядок');
-    break;
-  case isNaN(+numOrStr):
-    console.log('Не число');
-    break;
-  default:
-    console.log('OK!');
-    break;
+if (isNaN(length)) {
+  alert('This is an invalid number. Try again.');
+} else if (length <= 3) {
+  alert('The length of the array must be greater than 3. Try again.');
+} else {
+  for (let i = 0; i < length; i++) {
+    let element = prompt('Enter an array element:');
+    arr.push(element);
+  }
+  alert(`Created array: ${arr}`);
+  arr.sort();
+  alert(`Sorted array:  ${arr}`);
+  arr.splice(1, 3);
+  alert(`Updated array:  ${arr}`);
 }
