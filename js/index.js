e880f97cbfe181bab5b8b182e1d26616aa80ab87
Tetiana Peleshko <t.peleshko@gmail.com>
2023-08-03 18:19:10 +0300

    HW15_Recursion

--- js/index.js
+++ js/index.js
@@ -1,105 +1,23 @@
-// 1. Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
+// створити піраміду фібоначі
 
-let arr = [
-  1,
-  2,
-  3,
-  77,
-  -5,
-  'ff',
-  'vfd',
-  undefined,
-  'd',
-  'j',
-  -9,
-  -12,
-  15,
-  true,
-  null,
-];
+function fibonachy(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return fibonachy(n - 1) + fibonachy(n - 2);
+  }
+}
 
-function averageNum(arr) {
-  let arrayOfNumbers = [];
-
-  for (const item of arr) {
-    if (!isNaN(item)) {
-      arrayOfNumbers.push(item);
+function triangle(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+      row += fibonachy(j) + ' ';
     }
+    console.log(row);
   }
-
-  return (
-    arrayOfNumbers.reduce((acc, num) => acc + num, 0) / arrayOfNumbers.length
-  );
 }
 
-let average = averageNum(arr);
-console.log(average);
+triangle(15);
 
-// 2. Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
-let x = Number(prompt('Enter number x:'));
-let y = Number(prompt('Enter number y:'));
-let znak = prompt('Enter znak:');
-
-function doMath(x, znak, y) {
-  switch (znak) {
-    case '+':
-      result = x + y;
-      break;
-    case '-':
-      result = x - y;
-      break;
-    case '*':
-      result = x * y;
-      break;
-    case '/':
-      result = x / y;
-      break;
-    case '%':
-      result = x % y;
-      break;
-    case '^':
-      result = x ** y;
-      break;
-    default:
-      alert('Wrong input');
-  }
-  // result = +result.toFixed(2);
-  return result;
-}
-
-let outputResult = doMath(x, znak, y);
-console.log(outputResult);
-
-// 3. Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач
-
-let lengthArr = Number(prompt('Enter array length:'));
-let heightArr = Number(prompt('Enter array height:'));
-
-function fillArray(lengthArr, heightArr) {
-  let filledArray = [];
-  for (let i = 0; i < lengthArr; i++) {
-    let internalArray = [];
-    for (let j = 0; j < heightArr; j++) {
-      internalArray.push(prompt(`Enter element number [${i}, ${j}]`));
-    }
-    filledArray.push(internalArray);
-  }
-  return filledArray;
-}
-
-let newArray = fillArray(lengthArr, heightArr);
-console.log(newArray);
-
-// 4. Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
-let hello = 'Hello world';
-let delChar = ['o', 'd'];
-
-function func(inpStr, delChar) {
-  let addToArray = inpStr.split('');
-  let filteredArray = addToArray.filter((char) => !delChar.includes(char));
-  let resultStr = filteredArray.join('');
-  return resultStr;
-}
-
-let newHello = func(hello, delChar);
-console.log(newHello);
+// В завдвнні написано "створити піраміду фібоначі", а на зображенні намальована піраміда паскаля. Я не зрозуміла, що саме треба порахувати, тож написала рекурсію на розрахунок фібоначі
