08001aaca5236ee5b60ae6da973e5fc2ada9c455
Tetiana Peleshko <t.peleshko@gmail.com>
2023-08-25 00:25:33 +0300

    HW21_Table

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-const textField = document.getElementById('textField');
-const blueSquare = document.getElementById('sideDiv');
-textField.addEventListener('focus', function () {
-  blueSquare.style.display = 'block';
-});
-textField.addEventListener('blur', function () {
-  blueSquare.style.display = 'none';
-});
+const tableBody = document.getElementById('tableBody');
+
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement('tr');
+
+  for (let j = 0; j < 10; j++) {
+    const cell = document.createElement('td');
+    const cellNum = i * 10 + j + 1;
+    cell.textContent = cellNum;
+    row.appendChild(cell);
+  }
+  setTimeout(() => tableBody.appendChild(row), 1000 * i); //Додала setTimeout щоб перевірити на дінамічність додавання
+}
