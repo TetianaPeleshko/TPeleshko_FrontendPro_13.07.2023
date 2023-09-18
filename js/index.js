66b22d6df344f585b6525eb195345d8fdf901797
Tetiana Peleshko <t.peleshko@gmail.com>
2023-09-18 19:48:53 +0300

    Practice1

--- js/index.js
+++ js/index.js
@@ -1,22 +1,69 @@
 'use strict';
+// приклад з лекції 17
+const server = http.createServer((req, res) => {
+  switch (req.url && req.method == 'GET') {
+    case '/':
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html);
+      break;
+    case '/users':
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(users));
+      break;
 
-function generateList(array) {
-  let result = '<ul>';
-  for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i])) {
-      result += `<li>${generateList(array[i])}</li>`;
-    } else {
-      result += `<li>${array[i]}</li>`;
-    }
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404: not found');
   }
-  result += '</ul>';
-  return result;
-}
+});
 
-const inputArray1 = [1, 2, 3];
-const inputArray2 = [1, 2, [1.1, 1.2, 1.3], 3];
-const htmlList1 = generateList(inputArray1);
-const htmlList2 = generateList(inputArray2);
+server.listen(8199, (_) => {
+  console.log(global);
+});
 
-// Додавання списку до DOM:
-document.body.innerHTML = `${htmlList1}${htmlList2}`;
+// приклад коду на Node.js  ми створюємо сервер, який слухає порт 8200 і відповідає на шлях "/" текстом "Hello, world!". Якщо шлях в запиті відрізняється від "/", сервер повертає 404: not found.
+('use strict');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, world!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404: not found');
+  }
+});
+
+const port = 8200; // Змініть порт на бажаний
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// У цьому коді ми використовуємо switch/case для обробки шляху запиту. Якщо шлях дорівнює "/", то сервер повертає "Hello, world!". В протилежному випадку, якщо шлях не визначений, сервер повертає 404: not found
+('use strict');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case '/':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello, world!');
+      break;
+
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404: not found');
+  }
+});
+
+const port = 8200; // Змініть порт на бажаний
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// У рядку адреси браузера введіть http://localhost:8200 (або відповідну адресу з вашим портом)
